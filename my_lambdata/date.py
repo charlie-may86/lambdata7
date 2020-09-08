aead815a98319d4adbb119e7776af362c1a97a61
Charlie May <camiv95@gmail.com>
2020-09-08 16:51:03 -0500

    cleaned up using pep8

--- my_lambdata/date.py
+++ my_lambdata/date.py
@@ -1,16 +1,14 @@
 import pandas as pd
 # Function to split dates ("MM/DD/YYYY", etc.) into multiple columns
 
+
 def date_columns(X):
-  '''
-  Change the name of your date columne to 'Date'
-  '''
+    '''
+    Change the name of your date columne to 'Date'
+    '''
 
-  #create a year column
-  X['year'] = pd.DatetimeIndex(X['Date']).year
+    # create a year column
+    X['year'] = pd.DatetimeIndex(X['Date']).year
 
-  #create a month column
-  X['month'] = pd.DatetimeIndex(X['Date']).month
-
-  #create a day column
-  X['day'] = pd.DatetimeIndex(X['Date']).day
+    # create a month column
+    X['month'] = pd.DatetimeIndex(X['Date']).month
