102bf3c0a6e8aaebd4499bccf1c7c50befdf7192
Charlie May <camiv95@gmail.com>
2020-09-09 13:57:20 -0500

    refactored ds_utilities code to be OOP

--- my_lambdata/ds_utilities.py
+++ my_lambdata/ds_utilities.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_wine
 from pdb import set_trace as breakpoint
+from IPython.display import display
 
 
 def enlarge(n):
@@ -12,39 +13,63 @@ def enlarge(n):
     return n * 100
 
 
-def train_validation_test_split(df, features, target,
-                                train_size=0.7, val_size=0.1,
-                                test_size=0.2, random_state=None,
-                                shuffle=True):
-    '''
-    This function is a utility wrapper around the Scikit-Learn train_test_split
-    that splits arrays or matrices into train, validation, and test subsets.
-    Args:
-        df (Pandas DataFrame): Dataframe with code.
-        X (list): A list of features.
-        y (str): A string with target column.
-        train_size (float or int): Proportion of the dataset to include in the
-        train split (0 to 1).
-        val_size (float or int): Proportion of the dataset to include in the
-        validation split (0 to 1).
-        test_size (float or int): Proportion of the dataset to include in the
-        test split (0 to 1).
-        random_state (int): Controls the shuffling applied to the data before
-        applying the split for reproducibility.
-        shuffle (bool): Whether or not to shuffle the data before splitting
-    Returns:
-        Train, test, and validation dataframes for features (X) and target (y).
-    '''
+class My_Data_Splitter():
+    def __init__(self, df, features, target):
+        self.df = df
+        self.features = features
+        self.target = target
+        self.X = df[features]
+        self.y = df[target]
 
-    X = df[features]
-    y = df[target]
+    def train_validation_test_split(self, train_size=0.7, val_size=0.1,
+                                    test_size=0.2, random_state=None,
+                                    shuffle=True):
+        '''
+        This function is a utility wrapper around the Scikit-Learn
+        train_test_split that splits arrays or matrices into train,
+        validation, and test subsets.
+        Args:
+            df (Pandas DataFrame): Dataframe with code.
+            X (list): A list of features.
+            y (str): A string with target column.
+            train_size (float or int): Proportion of the dataset to include in
+            the train split (0 to 1).
+            val_size (float or int): Proportion of the dataset to include in
+            the validation split (0 to 1).
+            test_size (float or int): Proportion of the dataset to include in
+            the test split (0 to 1).
+            random_state (int): Controls the shuffling applied to the data
+            before applying the split for reproducibility.
+            shuffle (bool): Whether or not to shuffle the data before splitting
+        Returns:
+            Train, test, validation dataframes for features (X) and target (y).
+        '''
 
-    X_train_val, X_test, y_train_val, y_test = train_test_split(
-        X, y, test_size=test_size, random_state=random_state, shuffle=shuffle)
-    X_train, X_val, y_train, y_val = train_test_split(
-        X_train_val, y_train_val, test_size=val_size / (train_size + val_size),
-        random_state=random_state, shuffle=shuffle)
-    return X_train, X_val, X_test, y_train, y_val, y_test
+        X_train_val, X_test, y_train_val, y_test = train_test_split(
+            self.X, self.y, test_size=test_size, random_state=random_state,
+            shuffle=shuffle)
+        X_train, X_val, y_train, y_val = train_test_split(
+            X_train_val, y_train_val,
+            test_size=val_size / (train_size + val_size),
+            random_state=random_state, shuffle=shuffle)
+        return X_train, X_val, X_test, y_train, y_val, y_test
+
+    def print_split_summary(self, X_train, X_val, X_test):
+
+        print('############## TRAINING DATA ###############')
+        print(f'X_train Shape: {X_train.shape}')
+        display(X_train.describe(include='all').transpose())
+        print('')
+
+        print('############## VALIDATION DATA ###############')
+        print(f'X_val Shape: {X_val.shape}')
+        display(X_val.describe(include='all').transpose())
+        print('')
+
+        print('############## VALIDATION DATA ###############')
+        print(f'X_test Shape: {X_test.shape}')
+        display(X_test.describe(include='all').transpose())
+        print('')
 
 # def something(x, y):
 #     """[summary]
@@ -75,3 +100,9 @@ if __name__ == '__main__':
     # train_validation_test_split(df, features=['ash', 'hue'], target='target')
 
     # breakpoint()
+    # test my data splitter
+    splitter = My_Data_Splitter(df=df, features=['ash', 'hue'],
+                                target='target')
+    X_train, X_val, X_test, y_train, y_val, y_test = splitter.train_validation_test_split()
+    splitter.print_split_summary(X_train, X_val, X_test)
+
